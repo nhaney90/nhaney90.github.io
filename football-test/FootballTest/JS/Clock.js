3be443a32c085ee960e0cb17b0f522813eadef80
Nicholas Haney <NHaney@esri.com>
2016-11-05 16:41:17 -0400

    Another edit

--- football-test/FootballTest/JS/Clock.js
+++ football-test/FootballTest/JS/Clock.js
@@ -7,29 +7,10 @@ define ([],function() {
 			this.timeRemaining = this.quarterLength;
 		}
 		
-		runTimer(startTime) {
-			this.currentTime = this.remainingTime - (((Date.now() - startTime)/1000) | 0);
-			var seconds = (this.currentTime % 60) | 0;
-			
-			seconds = seconds < 10 ? "0" + seconds : seconds;
-			if(this.currentTime <= 0) startTime = Date.now() + 1000;
-			console.log(this.currentTime);
-		}
-		
 		startTime() {
-			var start = Date.now();
-			this.remainingTime = this.currentTime;
-			(function(drive) {
-				drive.interval = setInterval(function() {
-					drive.runTimer(start);
-				}, 1000);
-			})(this);
-			this.started = true;
 		}
 		
 		stopTime() {
-			clearInterval(this.interval);
-			this.started = false;
 		}
 		
 		getCurrentTime() {
